e81ab8c1271cd989cd3a84c6de9a313cb5fc14c4
luke-bhan <luke.bhan@vanderbilt.edu>
2020-01-27 22:09:58 -0600

    added methods in card.py

--- card.py
+++ card.py
@@ -1,24 +1,26 @@
+import collections
+
 class card:
-    def __init__(self, suit = None, number = None):
-        if suit is None and number is None:
-            self.suit = ""
-            self.number = 0;
+    # Initializes a card and checks for intial value errors. A card without the suit or number is
+    # a useless. 
+    def __init__(self, suit, number):
+        if suit.lower() != "spades" and suit.lower() != "hearts" and suit.lower() != "clubs" \
+            and suit.lower() != "diamonds": 
+            raise ValueError('Enter a valid suit')
+        elif number > 13 or number < 2:
+            raise ValueError('Enter a valid number')
         else:
-            if suit.lower() != "spades" and suit.lower() != "hearts" and suit.lower() != "clubs" \
-                and suit.lower() != "diamonds": 
-                raise ValueError('Enter a valid suit')
-            elif number > 13 or number < 2:
-                raise ValueError('Enter a valid number')
-            else:
-                self.suit = suit
-                self.number = number
+            self.suit = suit
+            self.number = number
 
     def get_card(self):
-
+        return collections.namedtuple('Card', [self.suit, self.number])
 
     def get_suit(self):
+        return self.suit
 
     def get_number(self):
+        return self.number
 
     
         
