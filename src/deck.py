b3ee9c06d8c1c11b8df3ffa465fd8d2f0b2e7206
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-02-05 19:59:47 -0600

    Added constructor, string, and print methods

--- src/deck.py
+++ src/deck.py
@@ -1,19 +1,33 @@
 # Currently added compile lines so that my test file would run
 # Obviously when you implemnent these, it will delete them
 
+from src.card import *
+
+
 class Deck:
     # builds inital deck of cards thats empty if no second param is passed. Otherwise construct full
     # deck.
-    def __init__(self, initialize = None):
-        print("compile line")
+    def __init__(self, initialize=None):
+        if initialize is None:
+            self.cards = []
+        else:
+            self.cards = []
+            for s in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+                for v in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]:
+                    self.cards.append(Card(s, v))
 
-    # returns the length of the deck
+            # Used to keep count of the number of cards in the deck
+            global number_cards
+            number_cards = 52
+
+    # Returns the number of cards in the deck
     def __length__(self):
-        print("compile line")
+        return self.number_cards
 
     # Prints the deck in a clean way
     def __str__(self):
-        print("compile line")
+        for c in self.cards:
+            c.__str__()
 
     # Checks if the deck contains a card
     def __contains__(self, card):
@@ -22,14 +36,17 @@ class Deck:
     # Inserts a card into the deck
     def insert(self, card):
         print("compile line")
+        number_cards += 1
 
     # Deletes a card from the deck
     def delete(self, card):
         print("compile line")
+        number_cards -= 1
 
     # Draws a card from the deck
     def draw_card(self):
         print("compile line")
+        number_cards -= 1
 
     # Returns the array containing the deck
     def return_deck(self):
@@ -39,9 +56,9 @@ class Deck:
     def pickup_cards(self, card):
         print("compile line")
 
-    # Returns if a deck is empty
-
+    # Returns true if a deck is empty
     def is_empty(self):
-
-    def is_empty(self):
-        print("compile line")
+        if number_cards == 0:
+            return True
+        else:
+            return False
