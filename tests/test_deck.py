23f0b63f38899ab6c8b29a6b183c943563fb0a1b
Alex Feeley <alexandra.feeley.lamb@vanderbilt.edu>
2020-02-07 16:14:51 -0600

    Commented out deck tests not yet used

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -30,56 +30,56 @@ class TestDeck(unittest.TestCase):
         self.assertRaises(Exception, deck.insert(self.card), "Did not raise exception when trying \
                 to overfill deck")
 
-    def test_delete(self):
-        deck = Deck(True)
-        deck.delete(self.card)
-        self.assertEqual(len(deck), 53, "Delete method did not update length size")
-
-    def test_delete_zero_cards(self):
-        deck = Deck()
-        self.assertRaises(Exception, deck.delete(self.card), "Delete method did not raise \
-                exeception if no cards in deck")
-
-    def test_delete_card_does_not_exist(self):
-        deck = Deck(True)
-        deck.delete(self.card)
-        self.assertRaises(Exception, deck.delete(self.card), "Delete method did not raise \
-                exception if card deleted is not in deck")
-
-    def test_draw_card(self):
-        deck = Deck(True)
-        self.assertIsInstance(type(self.card), deck.draw_card(), "Draw Card did not return a card type")
-
-    def test_draw_card_size(self):
-        deck = Deck(True)
-        deck.draw_card()
-        self.assertEqual(len(deck), 53, "Deck size not updated on draw_card")
-
-    def test_return_deck(self):
-        deck = Deck(True)
-        a = []
-        a.append(self.card)
-        self.assertIsInstance(type(a), deck.return_deck())
-
-    def test_pickup_cards(self):
-        deck = Deck(True)
-        deck.pickup_cards(card)
-        self.assertNotEqual(52, len(deck), "Deck size did not shrink when pickup cards was called")
-
-    def test_pickup_cards_type(self):
-        a = []
-        a.append(self.card)
-        deck = Deck(True)
-        self.assertIsInstance(type(a), deck.pickup_cards(self.card), "Pickup cards did not return \
-                a list")
-
-    def test_is_empty(self):
-        deck = Deck(True)
-        self.assertFalse(deck.is_empty(), "failed to assert false when deck is full")
-
-    def test_is_empty_when_empty(self):
-        deck = Deck()
-        self.assertTrue(deck.is_empty(), "failed to assert true when deck is empty")
+    # def test_delete(self):
+    #     deck = Deck(True)
+    #     deck.delete(self.card)
+    #     self.assertEqual(len(deck), 53, "Delete method did not update length size")
+    #
+    # def test_delete_zero_cards(self):
+    #     deck = Deck()
+    #     self.assertRaises(Exception, deck.delete(self.card), "Delete method did not raise \
+    #             exeception if no cards in deck")
+    #
+    # def test_delete_card_does_not_exist(self):
+    #     deck = Deck(True)
+    #     deck.delete(self.card)
+    #     self.assertRaises(Exception, deck.delete(self.card), "Delete method did not raise \
+    #             exception if card deleted is not in deck")
+    #
+    # def test_draw_card(self):
+    #     deck = Deck(True)
+    #     self.assertIsInstance(type(self.card), deck.draw_card(), "Draw Card did not return a card type")
+    #
+    # def test_draw_card_size(self):
+    #     deck = Deck(True)
+    #     deck.draw_card()
+    #     self.assertEqual(len(deck), 53, "Deck size not updated on draw_card")
+    #
+    # def test_return_deck(self):
+    #     deck = Deck(True)
+    #     a = []
+    #     a.append(self.card)
+    #     self.assertIsInstance(type(a), deck.return_deck())
+    #
+    # def test_pickup_cards(self):
+    #     deck = Deck(True)
+    #     deck.pickup_cards(card)
+    #     self.assertNotEqual(52, len(deck), "Deck size did not shrink when pickup cards was called")
+    #
+    # def test_pickup_cards_type(self):
+    #     a = []
+    #     a.append(self.card)
+    #     deck = Deck(True)
+    #     self.assertIsInstance(type(a), deck.pickup_cards(self.card), "Pickup cards did not return \
+    #             a list")
+    #
+    # def test_is_empty(self):
+    #     deck = Deck(True)
+    #     self.assertFalse(deck.is_empty(), "failed to assert false when deck is full")
+    #
+    # def test_is_empty_when_empty(self):
+    #     deck = Deck()
+    #     self.assertTrue(deck.is_empty(), "failed to assert true when deck is empty")
 
 
 if __name__ == '__main__':
